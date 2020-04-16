48f9452de8a94079a3deba68936c35f1e64c14c5
Joshua Rakovitsky <josh.rakovitsky@outlook.com>
2020-04-16 00:27:13 -0400

    Initital Position Tracking Odometry Math

--- 90819A/src/main.cpp
+++ 90819A/src/main.cpp
@@ -1,4 +1,18 @@
 #include "main.h"
+#include <cmath>
+
+#define _USE_MATH_DEFINES
+
+pros::Controller controller(CONTROLLER_MASTER);
+
+pros::Motor leftFrontMotor(18, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
+pros::Motor leftBackMotor(20, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
+pros::Motor rightFrontMotor(12, MOTOR_GEARSET_18, true, MOTOR_ENCODER_DEGREES);
+pros::Motor rightBackMotor(19, MOTOR_GEARSET_18, true, MOTOR_ENCODER_DEGREES);
+
+pros::ADIEncoder leftEncoder('A', 'B', false);
+pros::ADIEncoder rightEncoder('C', 'D', false);
+pros::ADIEncoder backEncoder('E', 'F', false);
 
 /**
  * A callback function for LLEMU's center button.
@@ -6,6 +20,8 @@
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
+
+ 
 void on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
@@ -58,7 +74,105 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+
+
+void autonomous() {
+	
+	double WHEEL_DIAMETER = 4; //inches
+	double DISTANCE_TO_LEFT_ENCODER = 7.25; //inches
+	double DISTANCE_TO_RIGHT_ENCODER = 7.25; //inches
+	double DISTANCE_TO_BACK_ENCODER = 5.5; //inches
+	
+	leftFrontMotor.set_brake_mode(MOTOR_BRAKE_BRAKE);
+	leftBackMotor.set_brake_mode(MOTOR_BRAKE_BRAKE);
+	rightFrontMotor.set_brake_mode(MOTOR_BRAKE_BRAKE);
+	rightBackMotor.set_brake_mode(MOTOR_BRAKE_BRAKE);
+
+	leftEncoder.reset();
+	rightEncoder.reset();
+	backEncoder.reset();
+
+	double previousLeftEncoderDegrees = 0;
+	double previousRightEncoderDegrees = 0;
+	double previousBackEncoderDegrees = 0;
+
+	double leftEncoderDegrees = 0;
+	double rightEncoderDegrees = 0;
+	double backEncoderDegrees = 0;
+
+	double leftEncoderDegreesDifference = 0;
+	double rightEncoderDegreesDifference = 0;
+	double backEncoderDegreesDifference = 0;
+
+	double leftEncoderRadians = 0;
+	double rightEncoderRadians = 0;
+	double backEncoderRadians = 0;
+
+	double leftEncoderDistance = 0;
+	double rightEncoderDistance = 0;
+	double backEncoderDistance = 0;
+
+	double deltaTheta = 0;
+	
+	double vectorComponentX = 0;
+	double vectorComponentY = 0;
+
+	double newVectorComponentX = 0;
+	double newVectorComponentY = 0;
+
+	double positionVector[2] = {0, 0};
+	double newVector[2] = {0, 0};
+
+	double x = 0;
+	double y = 0;
+	double r = 0;
+	double theta = 0;
+	double newX = 0;
+	double newY = 0;
+
+	while (true){
+
+		leftEncoderDegrees = leftEncoder.get_value();
+		rightEncoderDegrees = rightEncoder.get_value();
+		backEncoderDegrees = backEncoder.get_value();
+
+		leftEncoderDegreesDifference = leftEncoderDegrees - previousLeftEncoderDegrees;
+		rightEncoderDegreesDifference = rightEncoderDegrees - previousRightEncoderDegrees;
+		backEncoderDegreesDifference = backEncoderDegrees - previousBackEncoderDegrees;
+
+		previousLeftEncoderDegrees = leftEncoderDegrees;
+		previousRightEncoderDegrees = rightEncoderDegrees;
+		previousBackEncoderDegrees = backEncoderDegrees;
+
+		leftEncoderDistance = leftEncoderDegreesDifference * M_PI / 180.0 * WHEEL_DIAMETER/2;
+		rightEncoderDistance = rightEncoderDegreesDifference * M_PI / 180.0 * WHEEL_DIAMETER/2;
+		backEncoderDistance = backEncoderDegreesDifference * M_PI / 180.0 * WHEEL_DIAMETER/2;
+
+		deltaTheta =  (leftEncoderDistance - rightEncoderDistance) / (DISTANCE_TO_LEFT_ENCODER + DISTANCE_TO_RIGHT_ENCODER);
+
+		x = 2 * sin(deltaTheta/2) * (backEncoderDistance / deltaTheta + DISTANCE_TO_BACK_ENCODER);
+		y = 2 * sin(deltaTheta/2) * (rightEncoderDistance / deltaTheta + DISTANCE_TO_RIGHT_ENCODER);
+
+		r = sqrt(pow(x, 2) + pow(y, 2));
+		theta = atan(y/x);
+
+		theta -= deltaTheta/2;
+
+		newX = r * cos(theta);
+		newY = r * sin(theta);
+
+		newVector[0] = newX;
+		newVector[1] = newY;		
+		
+		positionVector[0] = positionVector[0] + newVector[0];
+		positionVector[1] = positionVector[1] + newVector[1];
+	}
+	
+}
+
+
+
+
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -78,7 +192,7 @@ void opcontrol() {
 	pros::Motor left_mtr(1);
 	pros::Motor right_mtr(2);
 
-	while (true) {
+	/*while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
@@ -88,5 +202,5 @@ void opcontrol() {
 		left_mtr = left;
 		right_mtr = right;
 		pros::delay(20);
-	}
+	}*/
 }
