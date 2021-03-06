6883329a15d34cbf9510e5837b6be0361229d2a2
Rohan Wadhwa <rohan.avatar@gmail.com>
2021-03-06 18:05:54 -0500

    Added ports for new bot

--- 90819A/src/init/devices.cpp
+++ 90819A/src/init/devices.cpp
@@ -5,18 +5,18 @@ pros::Controller controller(CONTROLLER_MASTER);
 pros::Controller controllerTwo(CONTROLLER_PARTNER);
 
 //Drivetrain motors
-pros::Motor leftFrontMotor(14, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
-pros::Motor leftBackMotor(11, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
-pros::Motor rightFrontMotor(17, MOTOR_GEARSET_18, true, MOTOR_ENCODER_DEGREES);
-pros::Motor rightBackMotor(20, MOTOR_GEARSET_18, true, MOTOR_ENCODER_DEGREES);
+pros::Motor leftFrontMotor(15, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
+pros::Motor leftBackMotor(4, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
+pros::Motor rightFrontMotor(7, MOTOR_GEARSET_18, true, MOTOR_ENCODER_DEGREES);
+pros::Motor rightBackMotor(10, MOTOR_GEARSET_18, true, MOTOR_ENCODER_DEGREES);
 
 //Stack motors
-pros::Motor lowerStack(1, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
-pros::Motor upperStack(10, MOTOR_GEARSET_18, true, MOTOR_ENCODER_DEGREES);
+pros::Motor lowerStack(20, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
+pros::Motor upperStack(12, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
 
 //Intake motors
-pros::Motor intakeMotorLeft(4, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
-pros::Motor intakeMotorRight(7, MOTOR_GEARSET_18, true, MOTOR_ENCODER_DEGREES);
+pros::Motor intakeMotorLeft(18, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
+pros::Motor intakeMotorRight(11, MOTOR_GEARSET_18, true, MOTOR_ENCODER_DEGREES);
 
 //Shaft encoders
 pros::ADIEncoder leftEncoder('A', 'B', true);
@@ -24,9 +24,9 @@ pros::ADIEncoder rightEncoder('G', 'H', true);
 pros::ADIEncoder backEncoder('E', 'F', true);
 
 //Inertial sensors
-pros::Imu imuLeft(5);
-pros::Imu imuRight(6);
-pros::Imu imuCenter(13);
+pros::Imu imuLeft(0);
+pros::Imu imuRight(0);
+pros::Imu imuCenter(0);
 
 //Ultrasonic range finders
 pros::ADIUltrasonic ultrasonicTopUpper({ {2, 'A', 'B'} });
@@ -35,7 +35,7 @@ pros::ADIUltrasonic ultrasonicBottomUpper({ {2, 'E', 'F'} });
 pros::ADIUltrasonic ultrasonicBottomLower({ {2, 'G', 'H'} });
 
 //Optical sensors
-pros::Optical opticalSensor(9);
+pros::Optical opticalSensor(0);
 
 //Limit switches
 pros::ADIDigitalIn limitSwitch('D');
